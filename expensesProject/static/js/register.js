766631fded767a3ce3cc7d4d7fb8b59164837650
bharathjinka09 <bharathjinka09@gmail.com>
2020-08-29 16:02:41 +0530

    Ajax username Validation

--- expensesProject/static/js/register.js
+++ expensesProject/static/js/register.js
@@ -1,10 +1,14 @@
 const usernameField = document.querySelector('#usernameField')
+const feedBackArea = document.querySelector('.invalid-feedback')
 
 usernameField.addEventListener('keyup',(e)=>{
 	
 	console.log('typing......')
 
 	const usernameVal = e.target.value
+
+	usernameField.classList.remove('is-invalid')
+	feedBackArea.style.display='none'
 
 	if (usernameVal.length > 0){
 		fetch('/authentication/validate-username',{
@@ -14,6 +18,11 @@ usernameField.addEventListener('keyup',(e)=>{
 			.then(response => response.json())
 			.then(data => {
 				console.log(data)
+				if (data.username_error){
+					usernameField.classList.add('is-invalid')
+					feedBackArea.style.display='block'
+					feedBackArea.innerHTML=`<p>${data.username_error}</p>`
+				}
 			})
 	}
 })
