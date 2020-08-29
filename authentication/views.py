f94197a2ea7d379f5c2aa65351578b344332e9ee
bharathjinka09 <bharathjinka09@gmail.com>
2020-08-29 15:46:19 +0530

    Username validation

---
+++ authentication/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.views import View
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+import json
+# Create your views here.
+
+
+class UsernameValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        username = data['username']
+
+        if not str(username).isalnum():
+            return JsonResponse({'username_error': 'Username should only contain letters and numbers'}, status=400)
+
+        if User.objects.filter(username=username).exists():
+        	return JsonResponse({'username_error':'Sorry. Username already taken.Please choose another one! '}, status=409)
+
+        return JsonResponse({'username_valid': True})
+
+
+class RegistrationView(View):
+    def get(self, request):
+        return render(request, "authentication/register.html")
