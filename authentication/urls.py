fefb98cc4980bfda3915809e560edf6639922fca
bharathjinka09 <bharathjinka09@gmail.com>
2020-08-31 16:56:28 +0530

    Reset password

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,13 @@
-from .views import RegistrationView, UsernameValidationView, EmailValidationView,VerificationView,LoginView,LogoutView
+from .views import RegistrationView, UsernameValidationView, EmailValidationView, VerificationView, LoginView, LogoutView, RequestResetEmailView,SetNewPasswordView
+
 from django.urls import path
+
 from django.views.decorators.csrf import csrf_exempt
-from django.utils.encoding import force_bytes,force_text,DjangoUnicodeDecodeError
-from django.utils.http import urlsafe_base64_encode,urlsafe_base64_decode
+
+from django.utils.encoding import force_bytes, force_text, DjangoUnicodeDecodeError
+
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+
 from django.contrib.sites.shortcuts import get_current_site
 
 
@@ -16,10 +21,15 @@ urlpatterns = [
 
     path('validate-username', csrf_exempt(UsernameValidationView.as_view()),
          name='validate-username'),
-    
+
     path('validate-email', csrf_exempt(EmailValidationView.as_view()),
          name='validate-email'),
 
     path('activate/<uidb64>/<token>', VerificationView.as_view(), name='activate'),
+
+    path('set-new-password/<uidb64>/<token>', SetNewPasswordView.as_view(), name='set-new-password'),
+
+    path('request-reset-email', RequestResetEmailView.as_view(),
+         name='request-reset-email'),
 
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -12,6 +12,8 @@ from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.utils.encoding import force_bytes, force_text, DjangoUnicodeDecodeError
 from django.contrib.sites.shortcuts import get_current_site
 import json
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+
 
 
 class EmailValidationView(View):
@@ -69,23 +71,14 @@ class RegistrationView(View):
                 user.set_password(password)
                 user.is_active = False
                 user.save()
-                # current_site = get_current_site(request)
-                # email_body = {
-                #     'user': user,
-                #     'domain': current_site.domain,
-                #     'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-                #     'token': account_activation_token.make_token(user),
-                # }
 
-                # link = reverse('activate', kwargs={
-                #                'uidb64': email_body['uid'], 'token': email_body['token']})
                 uidb64 = urlsafe_base64_encode(force_bytes(user.pk))
-                domain = get_current_site(request).domain
+                current_site = get_current_site(request).domain
                 link = reverse('activate', kwargs={
                     'uidb64': uidb64, 'token': account_activation_token.make_token(user)
                 })
 
-                activate_url = 'http://' + domain + link
+                activate_url = 'http://' + current_site + link
 
                 email_body = f"Hi {user.username}. Please click this link to verify your account!\n {activate_url}"
 
@@ -159,8 +152,58 @@ class LoginView(View):
 
 
 class LogoutView(View):
-    def post(self,request):
+    def post(self, request):
         auth.logout(request)
-        messages.success(request,'You have been logged out!')
+        messages.success(request, 'You have been logged out!')
         return redirect('login')
-        
+
+
+class RequestResetEmailView(View):
+    def get(self, request):
+        return render(request, 'authentication/request-reset-email.html')
+
+    def post(self, request):
+        email = request.POST['email']
+
+        if not validate_email(email):
+            messages.error(request, 'Please enter a valid email')
+
+            return render(request, 'authentication/request-reset-email.html')
+
+        user = User.objects.filter(email=email)
+
+        if user.exists():
+            uidb64 = urlsafe_base64_encode(force_bytes(user[0].pk))
+            current_site = get_current_site(request).domain
+
+            link = reverse('request-reset-email', kwargs={
+                'uidb64': uidb64, 'token': PasswordResetTokenGenerator.make_token(user)
+            })
+
+            activate_url = 'http://' + current_site + link
+
+            email_subject = 'Reset your password'
+
+            email_body = f"Hi {user.username}. Please click this link to reset your password!\n {activate_url}"
+
+            # activate_url = 'http://'+current_site.domain+link
+
+            email = EmailMessage(
+                email_subject,
+                email_body,
+                sender_email,
+                [email],
+            )
+            email.send(fail_silently=False)
+
+        messages.success(
+            request, 'We have sent you an email with instructions on resetting your password')
+        return render(request, 'authentication/request-reset-email.html')
+
+
+class SetNewPasswordView(View):
+    def get(self, request, uidb64, token):
+        return render(request, 'authentication/request-reset-email.html')
+    
+    def post(self, request, uidb64, token):
+        return render(request, 'authentication/request-reset-email.html')
