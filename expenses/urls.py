3457c448e77ecc8af62354ac05db11cc866342c3
bharathjinka09 <bharathjinka09@gmail.com>
2020-09-01 20:17:56 +0530

    Getting data from input

--- expenses/urls.py
+++ expenses/urls.py
@@ -4,4 +4,5 @@ from .import views
 urlpatterns = [
     path('', views.index, name='expenses'),
     path('add-expense', views.add_expense, name='add-expense'),
+    path('grab-data', views.grab_data, name='grab-data'),
 ]
--- expenses/views.py
+++ expenses/views.py
@@ -8,6 +8,22 @@ from django.contrib.auth.decorators import login_required
 def index(request):
     return render(request, 'expenses/index.html')
 
+
 @login_required(login_url='/authentication/login')
 def add_expense(request):
     return render(request, 'expenses/add_expense.html')
+
+
+@login_required(login_url='/authentication/login')
+def grab_data(request):
+
+    url = request.POST.get('url', '')
+    title = request.POST.get('title', '')
+    article = request.POST.get('article', '')
+    author = request.POST.get('author', '')
+    pageno = request.POST.get('pageno', '')
+
+    abstract = request.POST.get('abstract', '')
+    keyword = request.POST.get('keyword', '')
+    print(url, title, article, author, pageno, abstract, keyword)
+    return render(request, 'expenses/grab_data.html')
